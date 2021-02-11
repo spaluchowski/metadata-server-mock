d9896b1951be32a4600932da759872af6e462550
Piotr Stachyra <piotr.stachyra@iohk.io>
2021-02-11 13:27:48 +0100

    Metadata Server Mock

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+
+class MetadataServerMock < Sinatra::Base
+
+  get "/" do
+    "MetadataServerMock ¯\_(ツ)_/¯"
+  end
+
+  get "/metadata/789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f1" do
+    content_type :json
+    JSON.parse(File.read("metadata/789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f1")).to_json
+  end
+
+  get "/metadata/789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f16861707079636f696e" do
+    content_type :json
+    JSON.parse(File.read("metadata/789ef8ae89617f34c07f7f6a12e4d65146f958c0bc15a97b4ff169f16861707079636f696e")).to_json
+  end
+
+end
